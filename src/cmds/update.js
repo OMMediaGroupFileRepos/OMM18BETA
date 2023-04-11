566b73f128c48b705d8e9b55d6cfcea2e509c721
ZionMedia <80281190+OMMediaGroupFileRepos@users.noreply.github.com>
2023-04-11 17:14:20 +0200

    Update update.js

--- src/cmds/update.js
+++ src/cmds/update.js
@@ -14,7 +14,7 @@ module.exports = {
 
       let latest = new EmbedBuilder()
             .setTitle("**Updating the bot...**\n** **\n")
-            .setDescription("**`Execute the command below to check what's new!`**\n```/updatelogs update:Latest```\n** **\n**The bot will check for changes and restarts, it might not change anything if there are no changes, we recommend you to follow our updates we announce in our official support server.**")
+            .setDescription("**`Execute the command below to see what's new!`**\n```/updatelogs update:Latest```\n** **\n*NOTE!*\n**The bot will check for changes and restarts, it might not change anything if there are no changes, we recommend you to follow our updates we announce in our official support server.**")
             .setFooter({ text: config.footer })
             .setTimestamp()
 
