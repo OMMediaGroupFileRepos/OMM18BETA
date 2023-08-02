cdd2ced92b81c1dbc4284b333c182cc97ed7908b
Mika W <djmika002@gmail.com>
2023-08-02 20:04:28 +0200

    Update lang.js

--- src/cmds/lang.js
+++ src/cmds/lang.js
@@ -4,7 +4,9 @@ const config = require("../data/config.json");
 const { exec } = require("child_process");
 
 var langConf = config.lang;
+var selectedLang = "";
 const l = require(`../lang/${langConf}.json`);
+const newL = require(`../lang/${selectedLang}.json`);
 
 module.exports = {
     category: "information",
@@ -30,7 +32,6 @@ module.exports = {
         
         var lang = "";
         var number = interaction.options.getString(l.langOption);
-        var selectedLang = "";
 
         if (number == "en_us") selectedLang = "en_us";
         if (number == "en_uk") selectedLang = "en_uk";
@@ -58,7 +59,6 @@ module.exports = {
             console.log(l.langUpdatedTextConsole + lang);
 
             langUpdatedText = "";
-            var l = require(`../lang/${selectedLang}.json`);
 
             if (selectedLang == "en_us") langUpdatedText = l.langUpdatedTo;
             if (selectedLang == "en_uk") langUpdatedText = l.langUpdatedTo;
