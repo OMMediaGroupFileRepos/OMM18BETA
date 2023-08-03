f2f071590644a99b40084059973caaee65b16a8a
Mika W <djmika002@gmail.com>
2023-08-03 19:34:32 +0200

    Update changes.js

--- src/cmds/changes.js
+++ src/cmds/changes.js
@@ -12,7 +12,6 @@ module.exports = {
         .setDescription(l.pingDesc),
     async execute(client, interaction) {
 
-        function getFormattedCommitMessages() {
             const commitHashes = exec('git log --pretty=format:"%H" origin/main..HEAD').toString().trim().split('\n');
             let formattedMessages = [];
 
@@ -21,28 +20,17 @@ module.exports = {
                 const commitMessage = commitInfo.split('\n\n')[1]; // Haal alleen het commitbericht op
                 const commitDate = commitInfo.split('\n\n')[0]; // Haal alleen de datum op
 
-                const change = {
-                    title: "**" + commitDate + "**",
-                    value: commitMessage
-                };
-
                 formattedMessages.push(change);
+                let embed = new EmbedBuilder()
+                .setTitle(commitDate)
+                .addFields(
+                    { name: commitTitle, value: commitMessage },
+            )
+                .setFooter({ text: config.footer })
+                .setColor("#80ddd9")
+                .setTimestamp()
+    
+             interaction.reply({ embeds: [embed], ephemeral: true })
             }
-
-            let embed = new EmbedBuilder()
-            .setTitle(commitDate)
-            .addFields(
-                { name: commitTitle, value: commitMessage },
-        )
-            .setFooter({ text: config.footer })
-            .setColor("#80ddd9")
-            .setTimestamp()
-
-         interaction.reply({ embeds: [embed], ephemeral: true })
-
-            //return formattedMessages.length > 0 ? formattedMessages : [{ title: "No content available", value: "" }];
-        }
-
-        //const changes = getFormattedCommitMessages();
     },
 };
